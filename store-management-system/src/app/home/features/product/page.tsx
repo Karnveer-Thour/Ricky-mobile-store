bcc9f4ed8a338e61cba8db3012e9049b7bb39151
K-Thour <karanveerthour76@gmail.com>
2025-06-08 19:13:34 +0530

    fix(product): mannual error removed

--- store-management-system/src/app/home/features/product/page.tsx
+++ store-management-system/src/app/home/features/product/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Product from './components/product'
 
 async function page() {
-  throw new Error("This is a server component, please use it as such.");
   return (
     <div>
       <Product />
