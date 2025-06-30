ae1cae08a74e37ddb097401fccc398d34ca56da4
K-Thour <karanveerthour76@gmail.com>
2025-06-30 21:38:14 +0530

    feat(dashboard): line chart implemented successfully

--- store-management-system/src/app/home/features/dashboard/page.tsx
+++ store-management-system/src/app/home/features/dashboard/page.tsx
@@ -1,8 +1,54 @@
 "use client";
 
+import SimpleLineChart from "@/components/charts/simplelinechart";
 import CountCard from "@/components/countcard";
 import { storeType } from "@/types/store.index";
 import { useSelector } from "react-redux";
+
+const lineChartData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 function page() {
   const isDark = useSelector((state: storeType) => state.DarkMode.isDarkMode);
@@ -25,9 +71,17 @@ function page() {
         </div>
       </div>
       <p className="text-center mt-1 text-3xl">Sales and customers</p>
-      <div className="h-100 mt-1 flex justify-center items-center">
+      <div className="h-150 mt-1 flex justify-center items-center">
         <div className="w-[95%] h-full flex justify-between items-center py-5"> 
-          <div className="shadow-md bg-white w-[50%] h-full rounded-2xl"></div>
+          <div className="shadow-md bg-white w-[50%] h-full rounded-2xl p-3">
+            <p className="mb-1 ms-3 text-2xl">Current year sales and customers</p>
+             <hr
+          className={`border-t-3 ${isDark ? "text-gray-100" : "text-gray-700"} mb-2 flex-1`}
+        ></hr>
+        <div className="h-[93%] w-full">
+            <SimpleLineChart isDark={isDark} data={lineChartData}/>
+        </div>
+          </div>
           <div className="shadow-md bg-white w-[40%] h-full rounded-2xl"></div>
         </div>
       </div>
---
+++ store-management-system/src/components/charts/simplelinechart.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+type chartData = {
+  name: string,
+  uv: number,
+  pv: number,
+  amt: number,
+}
+
+interface lineChartProps {
+  data: chartData[],
+  isDark?: boolean,
+}
+
+function SimpleLineChart({data,isDark}:lineChartProps) {
+   return (
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={250}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+}
+
+export default SimpleLineChart
